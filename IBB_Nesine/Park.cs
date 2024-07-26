5ce82410984ba0d76beeb3be09d0dbd7369055ac
fozduran <fozduran@NSN-FOZDURAN>
2024-07-26 10:44:55 +0300

    Add project files.

---
+++ IBB_Nesine/Park.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBB_Nesine
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string ParkName { get; set; }
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+        public int Capacity { get; set; }
+        public int EmptyCapacity { get; set; }
+        public string WorkHours { get; set; }
+        public string ParkType { get; set; }
+        public int FreeTime { get; set; }
+        public string District { get; set; }
+        public int IsOpen { get; set; }
+    }
+}
---
+++ IBB_Nesine/Program.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using IBB_Nesine;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+var apiUrl = "https://api.ibb.gov.tr/ispark/Park";
+var connectionString = "Server=NSN-FOZDURAN\\SQLEXPRESS;Database=dapper;Trusted_connection=True;MultipleActiveResultSets=true;";
+
+using (HttpClient client = new())
+{
+    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+    if (response.IsSuccessStatusCode)
+    {
+        string content = await response.Content.ReadAsStringAsync();
+
+        List<Park> parks = JsonConvert.DeserializeObject<List<Park>>(content);
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var sp = "usp_AddPark";
+            foreach (Park park in parks)
+            {
+                connection.Execute(sp, new
+                {
+                    park.ParkId,
+                    park.ParkName,
+                    park.Lat,
+                    park.Lng,
+                    park.Capacity,
+                    park.EmptyCapacity,
+                    park.WorkHours,
+                    park.ParkType,
+                    park.FreeTime,
+                    park.District,
+                    park.IsOpen
+                },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}

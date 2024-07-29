d732a9aabe626eff840609dce957c02a3fa3aa3b
Faruk Furkan Özduran <faruk.ozduran@nesine.com>
2024-07-29 08:46:02 +0300

    API added

--- IBB_Nesine/Program.cs
+++ IBB_Nesine/Program.cs
@@ -3,6 +3,7 @@ using IBB_Nesine;
 using Newtonsoft.Json;
 using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
 
 var apiUrl = "https://api.ibb.gov.tr/ispark/Park";
 var connectionString = "Server=NSN-FOZDURAN\\SQLEXPRESS;Database=dapper;Trusted_connection=True;MultipleActiveResultSets=true;";
@@ -41,5 +42,27 @@ using (HttpClient client = new())
             }
         }
 
+        var parameters = new DynamicParameters();
+
+        using(var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            foreach (var park in parks)
+            {
+                parameters.Add("@ParkId", park.ParkId);
+                parameters.Add("@ParkName", park.ParkName);
+                parameters.Add("@Lat", park.Lat);
+                parameters.Add("@Lng", park.Lng);
+                parameters.Add("@Capacity", park.Capacity);
+                parameters.Add("@EmptyCapacity", park.EmptyCapacity);
+                parameters.Add("@WorkHours", park.WorkHours);
+                parameters.Add("@ParkType", park.ParkType);
+                parameters.Add("@FreeTime", park.FreeTime);
+                parameters.Add("@District", park.District);
+                parameters.Add("@IsOpen", park.IsOpen);
+                connection.Execute("usp_AddPark", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }        
     }
 }
---
+++ IBB_Nesine_API/Controllers/ParksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using System.Data;
+using IBB_Nesine;
+
+namespace IBB_Nesine_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParksController : Controller
+    {
+        private readonly IDbConnection _db;
+        
+        public ParksController(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("GetParkByDistrict")]
+        public List<Park> GetParkByDistrict(string district)
+        {
+            var sql = "SELECT * FROM Parks WHERE District = @District";
+            var parks = _db.Query<Park>(sql, new {District = district}).ToList();
+            return parks;            
+        }
+    }
+}
